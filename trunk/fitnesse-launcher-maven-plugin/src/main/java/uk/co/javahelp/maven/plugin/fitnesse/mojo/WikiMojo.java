3c64f837bf0b72af90632f14fb47d4e54f27b8df
stewart.cambridge@gmail.com <stewart.cambridge@gmail.com@b9679af3-47a1-7dbf-88ed-62cb0186c6c5>
2012-05-15 20:22:54 +0000

    Refine plugin POM configuration, and provide content for an auto-generated 'help' mojo.

--- trunk/fitnesse-launcher-maven-plugin/src/main/java/uk/co/javahelp/maven/plugin/fitnesse/mojo/WikiMojo.java
+++ trunk/fitnesse-launcher-maven-plugin/src/main/java/uk/co/javahelp/maven/plugin/fitnesse/mojo/WikiMojo.java
@@ -6,15 +6,21 @@ import org.apache.maven.plugin.MojoFailureException;
 import uk.co.javahelp.maven.plugin.fitnesse.util.Interrupter;
 
 /**
- * Unfortunately, the FitNesse API does not expose a way to stop the wiki server gracefully.
- * The object / method we need access to is {@link fitnesse.FitNesse.stop()}.
- * The intended use is for the user to press Ctrl+C to quit.
+ * Goal that launches FitNesse as a wiki server.
+ * Useful for manually running / developing / debugging FitNesse tests.
+ * Once launched, just visit http://localhost:&lt;port&gt;/&lt;suite&gt;.
+ * Use Ctrl+C to shutdown.
  *
  * @goal wiki
  * @requiresDependencyResolution
  */
 public class WikiMojo extends AbstractMojo {
 
+    /**
+	 * Unfortunately, the FitNesse API does not expose a way to stop the wiki server gracefully.
+	 * The object / method we need access to is {@link fitnesse.FitNesse.stop()}.
+	 * The intended use is for the user to press Ctrl+C to quit.
+	 */
     public void executeInternal() throws MojoExecutionException, MojoFailureException {
         try {
         	Runtime.getRuntime().addShutdownHook(new Interrupter(Thread.currentThread(), 0L));
