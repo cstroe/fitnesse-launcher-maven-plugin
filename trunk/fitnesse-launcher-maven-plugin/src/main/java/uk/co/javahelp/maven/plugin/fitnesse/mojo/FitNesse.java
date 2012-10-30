7d973e11ee5fdb99ce3063c17fcac332e6f2eb05
stewart.cambridge@gmail.com <stewart.cambridge@gmail.com@b9679af3-47a1-7dbf-88ed-62cb0186c6c5>
2012-10-30 23:19:52 +0000

    Upgrade clean-up mojos to generic setUp and tearDown mojos, which include unpacking FitNesse and moving it to the designated working directory, to help make pom.xml simpler in end-user projects

---
+++ trunk/fitnesse-launcher-maven-plugin/src/main/java/uk/co/javahelp/maven/plugin/fitnesse/mojo/FitNesse.java
@@ -0,0 +1,10 @@
+package uk.co.javahelp.maven.plugin.fitnesse.mojo;
+
+public interface FitNesse {
+	
+	static final String groupId = "org.fitnesse";
+
+	static final String artifactId = "fitnesse";
+	
+	static final String artifactKey = groupId + ":" + artifactId;
+}
