da8894c47a1186195f0cdeec960564a0a0c309ca
stewart.cambridge <stewart.cambridge@gmail.com>
2012-12-20 15:46:37 +0000

    Configure failsafe integration tests to use external proejct `fitnesse-launcher-java-example` rather than an embedded in-source faux project

--- fitnesse-launcher-maven-plugin/src/test/java/uk/co/javahelp/maven/plugin/fitnesse/mojo/ClasspathIT.java
+++ fitnesse-launcher-maven-plugin/src/test/java/uk/co/javahelp/maven/plugin/fitnesse/mojo/ClasspathIT.java
@@ -1,9 +1,14 @@
 package uk.co.javahelp.maven.plugin.fitnesse.mojo;
 
+import static java.lang.System.getProperty;
+import static java.util.Arrays.asList;
+
 import java.io.File;
+import java.io.IOException;
 
 import org.apache.maven.it.Verifier;
-import org.apache.maven.it.util.ResourceExtractor;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -11,18 +16,39 @@ import org.junit.Test;
  */
 public class ClasspathIT 
 {
+	private static String groupId = getProperty("it.groupId");
+	
+	private static String artifactId = getProperty("it.artifactId");
+	
+	private static String version = getProperty("it.version");
+	
+    private static File testDir = new File(getProperty("maven.test.tmpdir"), artifactId + "-" + version);	
+    
+	@Before
+	public void setUp() {
+		printf("Testing complete project %s:%s:%s ...", groupId, artifactId, version);
+	}
+	
+	@After
+	public void tearDown() throws IOException {
+		printf("Output available in %s", new File(testDir, "log.txt").getCanonicalPath());
+	}
+	
 	@Test
 	public void testRunTests() throws Exception {
-        File testDir = ResourceExtractor.extractResourcePath(ClasspathIT.class, "/it",
-        	new File(System.getProperty("maven.test.tmpdir", System.getProperty("java.io.tmpdir"))), true);
-
         Verifier verifier = new Verifier(testDir.getAbsolutePath(), null, true, false );
-        verifier.deleteArtifacts( "uk.co.javahelp.fitnesse", "fitnesse-launcher-it", "1.1.0-SNAPSHOT" );
+        verifier.deleteArtifacts(groupId, artifactId, version);
         
-        verifier.executeGoal("install");
+		printf("mvn clean install -P auto");
+        verifier.setCliOptions(asList("-P auto"));
+        verifier.executeGoals(asList("clean", "install"));
         
         verifier.verifyErrorFreeLog();
-        verifier.verifyTextInLog("21 right, 0 wrong, 0 ignored, 0 exceptions");
-        verifier.assertArtifactPresent( "uk.co.javahelp.fitnesse", "fitnesse-launcher-it", "1.1.0-SNAPSHOT", "jar" );
+        verifier.verifyTextInLog("38 right, 0 wrong, 0 ignored, 0 exceptions");
+        verifier.assertArtifactPresent(groupId, artifactId, version, "jar");
     }
+		
+	private void printf(String string, Object... args) {
+		System.out.println(String.format(string, args));
+	}
 }
