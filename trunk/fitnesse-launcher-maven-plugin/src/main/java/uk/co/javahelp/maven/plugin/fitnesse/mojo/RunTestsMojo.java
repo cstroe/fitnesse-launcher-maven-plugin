e34b16e1eb7c0c821e147fdb9b70359282e3cb01
stewart.cambridge@gmail.com <stewart.cambridge@gmail.com@b9679af3-47a1-7dbf-88ed-62cb0186c6c5>
2013-05-30 19:34:42 +0000

    The httpclient dependency included is of version 4.0.2, 			        which is incompatible with the version of httpclient pulled in 			        by Xebium in the webapp-example project, which is 4.1.2. 			        The result is a hidden 			        NoClassDefFoundError: org/apache/http/conn/scheme/SchemeSocketFactory 			        which causes Firefox to silently fail to boot from Selenium 			        and hence no Xebium based tests get run under the RunTestsMojo. 			        Ultimately we probably need to implement a custom-scoped 			        org.codehaus.plexus.classworlds.realm.ClassRealm 			        with which to boot FitNesse, but this is difficult as 			        the threads it launches are without any public access or interface, 			        meaning a custom FitNesseMain class might need to be written. 			        This may be desirable anyway, as we also have trouble identifying 			        the FitNesse thread to join in WikiMojo. 			        For now, as reporting functionality is not yet implemented, 			        an exclusion will do! 			        http://maven.apache.org/guides/mini/guide-maven-classloading.html

--- trunk/fitnesse-launcher-maven-plugin/src/main/java/uk/co/javahelp/maven/plugin/fitnesse/mojo/RunTestsMojo.java
+++ trunk/fitnesse-launcher-maven-plugin/src/main/java/uk/co/javahelp/maven/plugin/fitnesse/mojo/RunTestsMojo.java
@@ -25,7 +25,7 @@ import fitnesse.responders.run.TestSummary;
 /**
  * Goal that runs FitNesse tests using fitnesse.junit.TestHelper.
  * Intended to be bound to the 'integration-test' phase.
- *
+ * 
  * @goal run-tests
  * @phase integration-test
  * @requiresDependencyResolution runtime
@@ -36,21 +36,21 @@ public class RunTestsMojo extends AbstractFitNesseMojo implements SurefireReport
 	@Override
     public void executeInternal() throws MojoExecutionException, MojoFailureException {
 
-   		if(this.createSymLink) {
-   			createSymLink();
+		if (this.createSymLink) {
+			createSymLink();
 		}
 
-        final TestSummary fitNesseSummary = runFitNesseTests();
-        getLog().info(fitNesseSummary.toString());
-        final FailsafeSummary failsafeSummary = convertAndReportResults(fitNesseSummary);
-        writeSummary(failsafeSummary);
-    }
-	
+		final TestSummary fitNesseSummary = runFitNesseTests();
+		getLog().info(fitNesseSummary.toString());
+		final FailsafeSummary failsafeSummary = convertAndReportResults(fitNesseSummary);
+		writeSummary(failsafeSummary);
+	}
+
 	/**
 	 * Creating a SymLink is easiest when FitNesse is running in 'wiki server' mode.
 	 */
-    private void createSymLink() throws MojoExecutionException {
-        final String portString = this.port.toString();
+	private void createSymLink() throws MojoExecutionException {
+		final String portString = this.port.toString();
 		try {
             this.fitNesseHelper.launchFitNesseServer(portString, this.workingDir, this.root, this.logDir);
 			this.fitNesseHelper.createSymLink(this.suite, this.test, this.project.getBasedir(), this.testResourceDirectory, this.port);
@@ -59,125 +59,131 @@ public class RunTestsMojo extends AbstractFitNesseMojo implements SurefireReport
 		} finally {
 			this.fitNesseHelper.shutdownFitNesseServer(portString);
 		}
-    }
-    
-    /**
+	}
+
+	/**
      * Strange side-effect behaviour:
      * If debug=false, FitNesse falls into wiki mode.
-     */
-    private TestSummary runFitNesseTests() throws MojoExecutionException {
-        final ResultsListener resultsListener = new DelegatingResultsListener(
+	 */
+	private TestSummary runFitNesseTests() throws MojoExecutionException {
+		final ResultsListener resultsListener = new DelegatingResultsListener(
                 new PrintTestListener(), new JUnitXMLTestListener( this.resultsDir.getAbsolutePath()));
         final TestHelper helper = new TestHelper(this.workingDir, this.reportsDir.getAbsolutePath(), resultsListener);
-        helper.setDebugMode(true);
-        
-        try {
-            final String[] pageNameAndType = this.fitNesseHelper.calcPageNameAndType(this.suite, this.test);
-            final TestSummary summary = helper.run(pageNameAndType[0], pageNameAndType[1], this.suiteFilter, this.excludeSuiteFilter, this.port);
-            return summary;
-        } catch (Exception e) {
-            throw new MojoExecutionException("Exception running FitNesse tests", e);
-        }
-    }
-	
+		helper.setDebugMode(true);
+
+		// ClassLoader oldClassLoader = Thread.currentThread().getContextClassLoader();
+		// org.codehaus.plexus.classworlds.realm.ClassRealm oldLookupRealm = container.setLookupRealm( this.realm );
+		try {
+			final String[] pageNameAndType = this.fitNesseHelper.calcPageNameAndType(this.suite, this.test);
+			// Thread.currentThread().setContextClassLoader( this.realm );
+			final TestSummary summary = helper.run(pageNameAndType[0], pageNameAndType[1], this.suiteFilter, this.excludeSuiteFilter, this.port);
+			return summary;
+		} catch (Exception e) {
+			throw new MojoExecutionException("Exception running FitNesse tests", e);
+		} finally {
+			// Thread.currentThread().setContextClassLoader( oldClassLoader );
+			// container.setLookupRealm( oldLookupRealm );
+		}
+	}
+
     private FailsafeSummary convertAndReportResults(final TestSummary summary) throws MojoFailureException {
         final RunResult result = new RunResult(summary.right, summary.exceptions, summary.wrong, summary.ignores);
 		SurefireHelper.reportExecution(this, result, getLog());
 		final FailsafeSummary failsafeSummary = new FailsafeSummary();
 		failsafeSummary.setResult(result.getForkedProcessCode());
 		return failsafeSummary;
-    }
-	
-    private void writeSummary(final FailsafeSummary summary) throws MojoExecutionException {
-        Writer writer = null;
-        try {
-            writer = new FileWriter(this.summaryFile);
-            FailsafeSummaryXpp3Writer xpp3Writer = new FailsafeSummaryXpp3Writer();
-            xpp3Writer.write(writer, summary);
-        } catch (IOException e) {
-            throw new MojoExecutionException("Exception writing Failsafe summary", e);
-        } finally {
-            close(writer);
-        }
-    }
+	}
 
-    // ------------------------------------------------------------------------
-    // See http://maven.apache.org/plugins/maven-surefire-plugin/test-mojo.html
-    // ------------------------------------------------------------------------
+    private void writeSummary(final FailsafeSummary summary) throws MojoExecutionException {
+		Writer writer = null;
+		try {
+			writer = new FileWriter(this.summaryFile);
+			FailsafeSummaryXpp3Writer xpp3Writer = new FailsafeSummaryXpp3Writer();
+			xpp3Writer.write(writer, summary);
+		} catch (IOException e) {
+            throw new MojoExecutionException("Exception writing Failsafe summary", e);
+		} finally {
+			close(writer);
+		}
+	}
+
+	// ------------------------------------------------------------------------
+	// See http://maven.apache.org/plugins/maven-surefire-plugin/test-mojo.html
+	// ------------------------------------------------------------------------
 
 	@Override
-    public boolean isSkipTests() {
-        return false;
-    }
+	public boolean isSkipTests() {
+		return false;
+	}
 
 	@Override
     public void setSkipTests(boolean skipTests) {}
 
 	@Override
-    public boolean isSkipExec() {
-        return false;
-    }
+	public boolean isSkipExec() {
+		return false;
+	}
 
 	@Override
     public void setSkipExec(boolean skipExec) {}
 
 	@Override
-    public boolean isSkip() {
-        return false;
-    }
+	public boolean isSkip() {
+		return false;
+	}
 
 	@Override
     public void setSkip(boolean skip) {}
 
 	@Override
-    public boolean isTestFailureIgnore() {
-        return true;
-    }
+	public boolean isTestFailureIgnore() {
+		return true;
+	}
 
 	@Override
     public void setTestFailureIgnore(boolean testFailureIgnore) {}
 
 	@Override
-    public File getBasedir() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	public File getBasedir() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public void setBasedir(File basedir) {
-        // TODO Auto-generated method stub
+	public void setBasedir(File basedir) {
+		// TODO Auto-generated method stub
 
-    }
-
-	@Override
-    public File getTestClassesDirectory() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	}
 
 	@Override
-    public void setTestClassesDirectory(File testClassesDirectory) {
-        // TODO Auto-generated method stub
-
-    }
-
-	@Override
-    public File getReportsDirectory() {
-        return this.reportsDir;
-    }
+	public File getTestClassesDirectory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public void setReportsDirectory(File reportsDirectory) {
-        this.reportsDir = reportsDirectory;
-    }
+	public void setTestClassesDirectory(File testClassesDirectory) {
+		// TODO Auto-generated method stub
+
+	}
 
 	@Override
-    public Boolean getFailIfNoTests() {
-        return this.failIfNoTests;
-    }
+	public File getReportsDirectory() {
+		return this.reportsDir;
+	}
 
 	@Override
-    public void setFailIfNoTests(final Boolean failIfNoTests) {
-        this.failIfNoTests = failIfNoTests;
+	public void setReportsDirectory(File reportsDirectory) {
+		this.reportsDir = reportsDirectory;
+	}
+
+	@Override
+	public Boolean getFailIfNoTests() {
+		return this.failIfNoTests;
+	}
+
+	@Override
+	public void setFailIfNoTests(final Boolean failIfNoTests) {
+		this.failIfNoTests = failIfNoTests;
 	}
 }
