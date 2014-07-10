bfb791dffd2b2191b785cee49f420a458bbc39ae
stewart <stewart@javahelp.co.uk>
2014-07-10 16:57:51 +0000

    Back merge /trunk:423-431 -> /branches/1.4.x (Windows filesystem minor fixes + site docs updates)

--- fitnesse-launcher-maven-plugin/src/main/java/uk/co/javahelp/maven/plugin/fitnesse/util/Utils.java
+++ fitnesse-launcher-maven-plugin/src/main/java/uk/co/javahelp/maven/plugin/fitnesse/util/Utils.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public final class Utils {
 
@@ -32,37 +33,50 @@ public final class Utils {
     	if(canonicalFrom.equals(canonicalTo)) {
     		return ".";
     	}
-    	final String[] partsFrom = canonicalFrom.split("/");
-    	final String[] partsTo = canonicalTo.split("/");
-    	if(partsFrom.length == 0 || partsTo.length == 0) {
-    		return canonicalTo;
+    	final String separator = Pattern.quote(File.separator);
+    	final List<String> roots = new ArrayList<String>();
+    	for(final File root : File.listRoots()) {
+    		final String[] rs = root.getCanonicalPath().split(separator);
+    		if(rs.length > 0) {
+    			roots.add(rs[0]);
+    		}
     	}
+    	final String[] partsFrom = canonicalFrom.split(separator);
+    	final String[] partsTo = canonicalTo.split(separator);
     	final List<String> listFrom = new ArrayList<String>(Arrays.asList(partsFrom));
     	final List<String> listTo = new ArrayList<String>(Arrays.asList(partsTo));
+    	listFrom.removeAll(roots);
+    	listTo.removeAll(roots);
+    	if(listFrom.isEmpty() || listTo.isEmpty()) {
+    		return canonicalTo;
+    	}
     	
+    	final List<String> itrFrom = new ArrayList<String>(listFrom);
+    	final List<String> itrTo = new ArrayList<String>(listTo);
     	boolean same = true;
     	int i = 0;
-    	while(same && i < partsFrom.length && i < partsTo.length) {
-    		same = partsFrom[i].equals(partsTo[i]);
+    	while(same && i < itrFrom.size() && i < itrTo.size()) {
+    		same = itrFrom.get(i).equals(itrTo.get(i));
     		if(same) {
     			listFrom.remove(0);
     			listTo.remove(0);
     			i++;
     		}
     	}
-    	
+
     	final StringBuilder relativePath = new StringBuilder();
     	final int fromSize = listFrom.size();
     	if(fromSize == partsFrom.length - 1) {
-       	    relativePath.append("/");
+       	    relativePath.append(File.separator);
     	} else {
     	    for(int j = 0 ; j < fromSize ; j++ ) {
-        		relativePath.append("../");
-    		}
+        	relativePath.append("..");
+       	    	relativePath.append(File.separator);
+   		}
     	}
     	for(final String part : listTo) {
         	relativePath.append(part);
-        	relativePath.append("/");
+        	relativePath.append(File.separator);
     	}
     	relativePath.deleteCharAt(relativePath.length() - 1);
     	return relativePath.toString();
